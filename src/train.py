e1c78fb417566e7e4add73ddefb3ceb8d1366268
Your Name <dharmarajmanikkam332@gmail.com>
2025-06-02 20:03:24 +0530

    Test GitHub Actions

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
-
+print("Model is created")
 # Save model
 with open("models/house_model.pkl", "wb") as f:
     pickle.dump(model, f)
